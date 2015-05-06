77fe0f64f2188f14c3b21206f9896117d96c3dba
railya666 <railya666@rambler.ru>
2015-05-06 10:37:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 
 require_once('include/common.inc.php');
 
+$users = array 
+(
+    array('name' => 'Sarah', 'surname' => 'Rever', 'age' => 32),    
+    array('name' => 'Chuck', 'surname' => 'Norris', 'age' => 142),
+    array('name' => 'Dart',  'surname' => 'Veider', 'age' => 356)
+);
+
 $vars = array
 (
     'headline' => 'Template Engine',
     'name' => 'Sarah'
+    'users' => $users
 );
 
-echo getView('index.html', $vars);
+/*echo getView('index.html', $vars);*/
 buildLayout('index.html', $vars);
